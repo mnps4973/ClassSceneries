21837931ef57457e35f0fd5c8e2d049ea9f28ed3
up201908209 <up201908209@fc.up.pt>
2023-05-29 01:22:40 +0100

    activity 1 extras done

--- src/Pages/Activity1/Activity1.js
+++ src/Pages/Activity1/Activity1.js
@@ -5,7 +5,10 @@ import Popup from '../../Components/Popup/Popup';
 
 import { FaUserCircle, FaArrowAltCircleLeft, FaVolumeUp } from 'react-icons/fa';
 
+var points = 0;
+var clicked = [];
 
+const min_points = 0;
 
 export default function Activity1(){
     const data1 = {
@@ -59,9 +62,11 @@ export default function Activity1(){
             <a className="btn" href="/classroom"><FaArrowAltCircleLeft className="chalkboard"/></a>
             <a className="btn"><FaUserCircle className="profile"/></a>
             <div className="activity1-explanation">
-                <h1>This is the beginning of the class when everyone is still coming.</h1>
-                <h1>How would you start the class?</h1>
-                <h1>Choose the most suitable options to start the day</h1>
+                <div className="activity1-question">
+                    <h1>This is the beginning of the class when everyone is still coming.</h1>
+                    <h1>How would you start the class?</h1>
+                    <h1>Choose the most suitable options to start the day</h1>
+                </div>
 
                 <Popup data={data1} />
                 <Popup data={data2} />
@@ -74,38 +79,60 @@ export default function Activity1(){
                 <Popup data={data9} />
 
                 <div className="grid-container">
-                    <a className="btn grid-item wrong" id="1"><p>Wait until everyone is in class</p></a>
-                    <a className="btn grid-item right" id="2"><p>Play a short game that the kids know</p></a>
-                    <a className="btn grid-item wrong" id="3"><p>Start distributing papers for the class</p></a>
-                    <a className="btn grid-item right" id="4"><p>Ask the kids how they are doing</p></a>
-                    <a className="btn grid-item wrong" id="5"><p>Ask about homework</p></a>
-                    <a className="btn grid-item wrong" id="6"><p>Go directly to the topic of the day</p></a>
-                    <a className="btn grid-item wrong" id="7"><p>“Hello, everyone! We have a test today!”</p></a>
-                    <a className="btn grid-item wrong" id="8"><p>“Hi, Laura! You look very nice today!”</p></a>
-                    <a className="btn grid-item right" id="9"><p>“Hello, Laura! How is your day going?”</p></a>
+                    <a className="btn grid-item" id="1"><p>Wait until everyone is in class</p></a>
+                    <a className="btn grid-item" id="2"><p>Play a short game that the kids know</p></a>
+                    <a className="btn grid-item" id="3"><p>Start distributing papers for the class</p></a>
+                    <a className="btn grid-item" id="4"><p>Ask the kids how they are doing</p></a>
+                    <a className="btn grid-item" id="5"><p>Ask about homework</p></a>
+                    <a className="btn grid-item" id="6"><p>Go directly to the topic of the day</p></a>
+                    <a className="btn grid-item" id="7"><p>“Hello, everyone! We have a test today!”</p></a>
+                    <a className="btn grid-item" id="8"><p>“Hi, Laura! You look very nice today!”</p></a>
+                    <a className="btn grid-item" id="9"><p>“Hello, Laura! How is your day going?”</p></a>
                 </div>
                 
                 
                 <a className="btn"><FaVolumeUp className="sound"/></a>
-            </div>            
+                <p id="points1" className="points">Points: {points}</p>
+            </div>          
         </div>
     );
 
 }
 function showPop(id){
+    let id_str = "" + id;
+
+    if(!(clicked.find(element => element == id))) {
+        if((id == 1 || id == 3 || id == 5 || id == 6 || id == 7 || id == 8)){
+            if (points > min_points) points -= 1;
+            if(document.getElementById(id_str)) document.getElementById(id_str).style.backgroundColor = 'red';
+        } else if (id == 2 || id == 4 || id == 9){
+            points += 2;    
+            if(document.getElementById(id_str)) document.getElementById(id_str).style.backgroundColor = 'green';
+        }
+        clicked.push(id);
+    }
+
     const show = 'pop' + id;
     document.getElementById(show).style.display = 'block';
+
+    
 }
 
 setTimeout(function(){
-    if(document.getElementById('1')) document.getElementById('1').addEventListener('click', (event) => showPop('1'), false);
-    if(document.getElementById('2')) document.getElementById('2').addEventListener('click', (event) => showPop('2'), false);
-    if(document.getElementById('3')) document.getElementById('3').addEventListener('click', (event) => showPop('3'), false);
-    if(document.getElementById('4')) document.getElementById('4').addEventListener('click', (event) => showPop('4'), false);
-    if(document.getElementById('5')) document.getElementById('5').addEventListener('click', (event) => showPop('5'), false);
-    if(document.getElementById('6')) document.getElementById('6').addEventListener('click', (event) => showPop('6'), false);
-    if(document.getElementById('7')) document.getElementById('7').addEventListener('click', (event) => showPop('7'), false);
-    if(document.getElementById('8')) document.getElementById('8').addEventListener('click', (event) => showPop('8'), false);
-    if(document.getElementById('9')) document.getElementById('9').addEventListener('click', (event) => showPop('9'), false);
+    if(document.getElementById('1')) document.getElementById('1').addEventListener('click', (event) => showPop(1), false);
+    if(document.getElementById('2')) document.getElementById('2').addEventListener('click', (event) => showPop(2), false);
+    if(document.getElementById('3')) document.getElementById('3').addEventListener('click', (event) => showPop(3), false);
+    if(document.getElementById('4')) document.getElementById('4').addEventListener('click', (event) => showPop(4), false);
+    if(document.getElementById('5')) document.getElementById('5').addEventListener('click', (event) => showPop(5), false);
+    if(document.getElementById('6')) document.getElementById('6').addEventListener('click', (event) => showPop(6), false);
+    if(document.getElementById('7')) document.getElementById('7').addEventListener('click', (event) => showPop(7), false);
+    if(document.getElementById('8')) document.getElementById('8').addEventListener('click', (event) => showPop(8), false);
+    if(document.getElementById('9')) document.getElementById('9').addEventListener('click', (event) => showPop(9), false);
 }, 1000);
+
+setInterval(function(){
+    if(document.getElementById('points1')){
+        document.getElementById('points1').innerHTML = 'Points: ' + points;
+    }
+}, 3000);
 
